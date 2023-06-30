916c2323319586c9ad1349b3254a947986b5fac5
choushen <jacob.mckenzie@icloud.com>
2023-06-30 03:05:46 +0100

    Removed random comment

--- lua/user/packer.lua
+++ lua/user/packer.lua
@@ -1,5 +1,3 @@
---vim.keymap.set("n", "<leader>pv", vim.cmd.Ex) This file can be loaded by calling `lua require('plugins')` from your init.vim
-
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
 
