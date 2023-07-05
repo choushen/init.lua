61c3434c955d60cabbb0e3860bc116dc3cbb6f21
choushen <jacob.mckenzie@icloud.com>
2023-07-05 22:54:57 +0100

    lazy config for mac

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,8 @@
+return {
+  "github/copilot.vim",
+  branch = "release",
+  dependencies = "nvim-lua/plenary.nvim",
+  config = function()
+    -- Additional configuration for GitHub Copilot can be added here
+  end,
+}
