4bad4aeddc8538cc97e20f25f912b716017f545d
choushen <jacob.mckenzie@icloud.com>
2023-07-23 03:10:20 +0100

    new branch for mac config

--- lua/core/packer.lua
+++ lua/core/packer.lua
@@ -4,61 +4,60 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-
     -- Packer can manage itself
     use 'wbthomason/packer.nvim'
 
     -- LSP Zero
     use {
-      'VonHeikemen/lsp-zero.nvim',
-      branch = 'v1.x',
-      requires = {
-        -- LSP Support
-        {'williamboman/mason.nvim'},
-        {'williamboman/mason-lspconfig.nvim'},
-        {'neovim/nvim-lspconfig'},
-        {'jose-elias-alvarez/null-ls.nvim'},
-        {'onsails/lspkind-nvim'},
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v1.x',
+        requires = {
+            -- LSP Support
+            { 'williamboman/mason.nvim' },
+            { 'williamboman/mason-lspconfig.nvim' },
+            { 'neovim/nvim-lspconfig' },
+            { 'jose-elias-alvarez/null-ls.nvim' },
+            { 'onsails/lspkind-nvim' },
 
-        -- Autocompletion
-        {'hrsh7th/nvim-cmp'},
-        {'hrsh7th/cmp-buffer'},
-        {'hrsh7th/cmp-path'},
-        {'saadparwaiz1/cmp_luasnip'},
-        {'hrsh7th/cmp-nvim-lsp'},
-        {'hrsh7th/cmp-nvim-lua'},
-        {'windwp/nvim-autopairs'},
-        {'windwp/nvim-ts-autotag'},
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },
+            { 'hrsh7th/cmp-buffer' },
+            { 'hrsh7th/cmp-path' },
+            { 'saadparwaiz1/cmp_luasnip' },
+            { 'hrsh7th/cmp-nvim-lsp' },
+            { 'hrsh7th/cmp-nvim-lua' },
+            { 'windwp/nvim-autopairs' },
+            { 'windwp/nvim-ts-autotag' },
 
-        -- Snippets
-        {'L3MON4D3/LuaSnip'},
-        {'rafamadriz/friendly-snippets'},
-      }
-    } 
+            -- Snippets
+            { 'L3MON4D3/LuaSnip' },
+            { 'rafamadriz/friendly-snippets' },
+        }
+    }
 
     -- Nvim Surround
     use({
-      "kylechui/nvim-surround",
-      tag = "*", -- Use for stability; omit to use `main` branch for the latest features
-      config = function()
-          require("nvim-surround").setup({
-              -- Configuration here, or leave empty to use defaults
-          })
-      end
+        "kylechui/nvim-surround",
+        tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+        config = function()
+            require("nvim-surround").setup({
+                -- Configuration here, or leave empty to use defaults
+            })
+        end
     })
 
     -- Nvim Dap
-    use {'mfussenegger/nvim-dap',
-        'rcarriga/nvim-dap-ui',}
-    
+    use { 'mfussenegger/nvim-dap',
+        'rcarriga/nvim-dap-ui', }
+
     -- -- Neo Dev
     -- use 'folke/neodev.nvim'
 
     -- Telescope
     use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.0',
-    -- or                            , branch = '0.1.x',
-    requires = { {'nvim-lua/plenary.nvim'} }
+        'nvim-telescope/telescope.nvim', tag = '0.1.0',
+        -- or                            , branch = '0.1.x',
+        requires = { { 'nvim-lua/plenary.nvim' } }
     }
 
     -- Treesitter
@@ -76,7 +75,7 @@ return require('packer').startup(function(use)
     use 'nvim-tree/nvim-web-devicons'
 
     -- Nvim-Tree
-    use ("nvim-tree/nvim-tree.lua")
+    use("nvim-tree/nvim-tree.lua")
 
     -- Lua Line
     use("nvim-lualine/lualine.nvim")
@@ -88,17 +87,17 @@ return require('packer').startup(function(use)
     use {
         "folke/which-key.nvim",
         config = function()
-          vim.o.timeout = true
-          vim.o.timeoutlen = 300
-          require("which-key").setup {
-            -- your configuration comes here
-            -- or leave it empty to use the default settings
-            -- refer to the configuration section below
-          }
+            vim.o.timeout = true
+            vim.o.timeoutlen = 300
+            require("which-key").setup {
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                -- refer to the configuration section below
+            }
         end
-      }
+    }
 
-    
+
     -- Null Ls
     -- use{
     --   ("jose-elias-alvarez/null-ls.nvim"),
@@ -107,10 +106,10 @@ return require('packer').startup(function(use)
 
 
     -- Nui
-    use ("MunifTanjim/nui.nvim")
+    use("MunifTanjim/nui.nvim")
 
     -- Bufferline
-    use {'akinsho/bufferline.nvim', tag = "*", requires = 'nvim-tree/nvim-web-devicons'}
+    use { 'akinsho/bufferline.nvim', tag = "*", requires = 'nvim-tree/nvim-web-devicons' }
 
     -- Undotree (Undo History)
     use("mbbill/undotree")
@@ -118,31 +117,30 @@ return require('packer').startup(function(use)
     -- Fugitive (Git)
     use("tpope/vim-fugitive")
 
-  -- Trouble (LSP Diagnostics)
+    -- Trouble (LSP Diagnostics)
     use {
-      "folke/trouble.nvim",
-      requires = { "nvim-tree/nvim-web-devicons" },
-      opts = {
-      },
+        "folke/trouble.nvim",
+        requires = { "nvim-tree/nvim-web-devicons" },
+        opts = {
+        },
     }
 
     -- Toggleterm
-    use {'akinsho/toggleterm.nvim'}
+    use { 'akinsho/toggleterm.nvim' }
 
     -- Colorizer
-    use {'norcalli/nvim-colorizer.lua'}
+    use { 'norcalli/nvim-colorizer.lua' }
 
-    -- Zen 
-    use {'folke/zen-mode.nvim'}
-    
+    -- Zen
+    use { 'folke/zen-mode.nvim' }
 
-      -- Theme
+
+    -- Theme
     use({
-    'rose-pine/neovim',
-    as = 'rose-pine',
-    config = function()
-      vim.cmd('colorscheme rose-pine')
-    end
+        'rose-pine/neovim',
+        as = 'rose-pine',
+        config = function()
+            vim.cmd('colorscheme rose-pine')
+        end
     })
-
 end)
